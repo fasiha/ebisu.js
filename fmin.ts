a4566a821e942251fb9f49fd73d7173415eaefca
Ahmed Fasih <wuzzyview@gmail.com>
2023-02-01 19:03:24 -0800

    Upgrade to Ebisu V2.1 (#19)
    * typescript 2.0 first
    
    * all but one test passing
    
    * did I do this right? point types to index.js
    
    * tests pass with slightly higher tolerance, and with stdlib
    
    * clang-format and micro-refactors
    
    * docstrings and let->const
    
    * clean up deps
    
    * esbuild
    
    * update readme, removed website (left forward)
    
    * exact same test.json as ebisu 2.1 python reference
    
    * bump version
    
    * include return values
    
    * use arrow fn; update build
    
    * cite scijs; create release npm script
    
    The latter will compile/test/build and ensure everything is committed before
    publish
    
    * add some assertions
    
    * build
    
    * changelog
    
    * pre-release

---
+++ fmin.ts
@@ -0,0 +1,18 @@
+export const fmin = require("minimize-golden-section-1d") as (objective: (x: number) => number,
+                                                              options: Partial<Options>, status?: {}|Status) => number;
+
+export interface Options {
+  tolerance: number;
+  lowerBound: number;
+  upperBound: number;
+  maxIterations: number;
+  guess: number;
+  initialIncrement: number;
+}
+
+export interface Status {
+  converged: boolean;
+  iterations: number;
+  minimum: number;
+  argmin: number;
+}
--- test.js
+++ test.js
@@ -3,32 +3,44 @@ var ebisu = require('./index');
 var fs = require('fs');
 var ref = JSON.parse(fs.readFileSync('test.json', 'utf8'));
 
+var std = require('@stdlib/math');
+var stdlibEbisu = {betaln: std.base.special.betaln, betafn: std.base.special.beta};
+var substackEbisu = ebisu.customizeMath(stdlibEbisu);
+
 function relerr(dirt, gold) { return (dirt === gold) ? 0 : Math.abs(dirt - gold) / Math.abs(gold); }
 function relerrs(dirts, golds) { return Math.max(...dirts.map((d, i) => relerr(d, golds[i]))); }
 
 test('verify halflife', t => {
   const hl = 20;
-  t.ok(Math.abs(ebisu.modelToPercentileDecay([2, 2, hl], .5, true) - hl) > 1e-2);
-  t.ok(relerr(ebisu.modelToPercentileDecay([2, 2, hl], .5, false, 1e-6), hl) < 1e-3)
-  t.throws(() => ebisu.modelToPercentileDecay([2, 2, hl], .5, false, 1e-150), 'unreachable tolerance causes throw');
+  t.ok(relerr(ebisu.modelToPercentileDecay([2, 2, hl], .5, 1e-6), hl) < 1e-3)
+  t.throws(() => ebisu.modelToPercentileDecay([2, 2, hl], .5, 1e-150), 'unreachable tolerance causes throw');
   t.end();
 });
 
 test('compare', (t) => {
-  for (let elt of ref) {
-    var [operation, prior, args, result] = elt;
-    var err;
-    if (operation === 'update') {
-      var jsres = ebisu.updateRecall(prior, ...args);
-      result = result.post
-      err = relerrs(jsres, result);
-    } else if (operation === 'predict') {
-      var jsres = ebisu.predictRecall(prior, ...args, true);
-      result = result.mean;
-      err = relerr(jsres, result);
+  var STDLIB_THRESH = 3e-3;
+  var SUBSTACK_THRESH = 3e-2;
+  for (const useSubstack of [true, false]) {
+    var THRESH = useSubstack ? SUBSTACK_THRESH : STDLIB_THRESH;
+    if (useSubstack) {
+      ebisu.customizeMath(substackEbisu)
+    } else {
+      ebisu.customizeMath(stdlibEbisu)
     }
-    var THRESH = 2e-3; // Should be lower, why not? FIXME
-    t.ok(err < THRESH, `err=${err}, actual=${jsres}, expected=${result}`);
+    for (let elt of ref) {
+      var [operation, prior, args, result] = elt;
+      var err;
+      if (operation === 'update') {
+        var jsres = ebisu.updateRecall(prior, ...args);
+        result = result.post
+        err = relerrs(jsres, result);
+      } else if (operation === 'predict') {
+        var jsres = ebisu.predictRecall(prior, ...args, true);
+        result = result.mean;
+        err = relerr(jsres, result);
+      }
+      t.ok(err < THRESH, `err=${err}, actual=${jsres}, expected=${result}`);
+    }
   }
   t.end();
 });
